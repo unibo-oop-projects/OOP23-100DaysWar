39c4b85439f24d553dd8b40c1848d7846a05f846
Riccardo Bartolini <riccardo.bartolini5@studio.unibo.it>
2024-10-15 11:37:02 +0200

    creation interface Position and its implementation PositionImpl

---
+++ src/main/java/it/unibo/The100DaysWar/commons/utilities/PositionImpl.java
@@ -0,0 +1,94 @@
+package it.unibo.the100dayswar.commons.utilities;
+/**
+ * Class that model the concept of the postion
+ */
+public class PositionImpl implements Position {
+
+    private int x;
+    private int y;
+
+    /**
+     * Construct from x,y coords.
+     * @param x coord
+     * @param y coord
+     */
+    public PositionImpl(final int x, final int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Construct from given position's coords.
+     * @param position to copy from
+     */
+    public PositionImpl(final Position position) {
+        this.x = position.getX();
+        this.y = position.getY();
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setX(int x) {
+        this.x=x;
+        
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setY(int y) {
+        this.y=y;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void set(final Position position) {
+        setX(getX() + position.getX());
+        setY(getY() + position.getY());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(final Object o) {
+        if (o instanceof PositionImpl) {
+            final PositionImpl positionObject = (PositionImpl) o;
+            return this.getX() == positionObject.getX()
+                && this.getY() == positionObject.getY();
+        }
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        return "(" + this.getX() + ", " + this.getY() + ")";
+    }
+
+
+
+}
