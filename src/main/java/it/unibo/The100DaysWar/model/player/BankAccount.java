d7a766dc19df13d54004613d14c0605c1310cb4f
gimboSpaziale <gianmarco.fabbri3@studio.unibo.it>
2024-10-15 20:50:22 +0200

    Interface of BankAccount

---
+++ src/main/java/it/unibo/The100DaysWar/model/player/BankAccount.java
@@ -0,0 +1,33 @@
+package it.unibo.the100dayswar.model.player;
+
+/**
+ * Interface to define the behavior of a bank account for managing a player's resources.
+ */
+public interface BankAccount {
+    /**
+     * Retrieves the current balance of the bank account.
+     *
+     * @return the current balance of resources
+     */
+    int getBalance();
+    /**
+     * Adds resources to the player's bank account.
+     *
+     * @param amount the amount of resources to add
+     */
+    void addResources(int amount);
+    /**
+     * Spends a specific amount of resources from the player's bank account.
+     *
+     * @param buyable the amount of resources to spend
+     * @throws IllegalStateException if the player does not have enough resources
+     */
+    void buy(int amount);
+    /**
+     * Checks if the bank account has enough resources for a specified amount.
+     *
+     * @param amount the amount of resources needed
+     * @return true if the player has enough resources, false otherwise
+     */
+    boolean canAfford(int amount);
+}
