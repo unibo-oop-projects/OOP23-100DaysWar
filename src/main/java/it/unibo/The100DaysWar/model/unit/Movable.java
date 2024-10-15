3c602384fd3db459a833161766763ca654b6bed8
gimboSpaziale <gianmarco.fabbri3@studio.unibo.it>
2024-10-15 11:12:11 +0200

    Interface Movable

---
+++ src/main/java/it/unibo/The100DaysWar/model/unit/Movable.java
@@ -0,0 +1,11 @@
+package it.unibo.the100dayswar.model.unit;
+
+/**
+ * An interface for the objects that can move around the map.
+ */
+public interface Movable {
+    /**
+     * Move the object to a new cell.
+     */
+    void move();
+}
