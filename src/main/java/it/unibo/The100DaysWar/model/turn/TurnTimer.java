dc9f6b315036ac02f17507b54769d20ba6abb549
Filippo <filippo.francalanci@studio.unibo.it>
2024-10-18 11:34:41 +0200

    created interface TurnTimer

---
+++ src/main/java/it/unibo/The100DaysWar/model/turn/TurnTimer.java
@@ -0,0 +1,15 @@
+package it.unibo.the100dayswar.model.turn;
+
+/**
+ * Interface for the timer in a turn.
+ */
+public interface TurnTimer {
+    /**
+     * function for starting the timer.
+     */
+    void startTimer();
+    /**
+     * function for stopping the timer.
+     */
+    void stopTimer();
+}
