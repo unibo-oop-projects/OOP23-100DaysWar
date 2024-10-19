12bfd1d65ba28b597fe191e3e153cbf2e2571ff7
gimboSpaziale <gianmarco.fabbri3@studio.unibo.it>
2024-10-19 15:48:01 +0200

    Changes on Unit and Soldier

--- src/main/java/it/unibo/the100dayswar/model/unit/Soldier.java
+++ src/main/java/it/unibo/the100dayswar/model/unit/Soldier.java
@@ -4,14 +4,14 @@ package it.unibo.the100dayswar.model.unit;
  * The phisical structure that can move around the map, attack other soldiers, towers and conquer territories.
  */
 public class Soldier extends Unit implements Movable {
+    private static final int SOLDIER_DEFAULT_COST = 50;
+    private static final int SOLDIER_DEFAULT_COST_TO_UPGRADE = 30;
+    private static final int SOLDIER_MAX_LEVEL = 3;
     /**
      * Constructor for the soldier.
-     * 
-     * @param baseCost the cost of the soldier.
-     * @param maxLevel the maximum level of the soldier.
      */
-    public Soldier(final int baseCost, final int maxLevel) {
-        super(baseCost, maxLevel, maxLevel, maxLevel, maxLevel);
+    public Soldier() {
+        super(SOLDIER_DEFAULT_COST, SOLDIER_DEFAULT_COST_TO_UPGRADE, SOLDIER_MAX_LEVEL);
     }
     /**
      * {@inheritDoc}
@@ -27,5 +27,13 @@ public class Soldier extends Unit implements Movable {
     @Override
     public void takeDamage(final int damage) {
         setHealth(this.currentHealth() - damage);
+    }
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void performAttack() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'performAttack'");
     }
 }
--- src/main/java/it/unibo/the100dayswar/model/unit/Unit.java
+++ src/main/java/it/unibo/the100dayswar/model/unit/Unit.java
@@ -5,6 +5,9 @@ package it.unibo.the100dayswar.model.unit;
  */
 public abstract class Unit implements Buyable, Combatant {
 
+    private static final int DEFAULT_HEALTH = 100;
+    private static final int DEFAULT_LEVEL = 1;
+
     private int health;
     private int level;
     private final int costToBuy;
@@ -14,15 +17,13 @@ public abstract class Unit implements Buyable, Combatant {
     /**
      * Constructor from the given parameters for a generic Unit.
      * 
-     * @param health health
-     * @param level level
      * @param costToBuy cost to buy
      * @param costToUpgrade cost to upgrade
      * @param maxLevel maximum level
       */
-    public Unit(final int health, final int level, final int costToBuy, final int costToUpgrade, final int maxLevel) {
-        this.health = health;
-        this.level = level;
+    public Unit(final int costToBuy, final int costToUpgrade, final int maxLevel) {
+        this.health = DEFAULT_HEALTH;
+        this.level = DEFAULT_LEVEL;
         this.costToBuy = costToBuy;
         this.costToUpgrade = costToUpgrade;
         this.maxLevel = maxLevel;
@@ -52,10 +53,7 @@ public abstract class Unit implements Buyable, Combatant {
      * {@inheritDoc}
      */
     @Override
-    public void performAttack() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'performAttack'");
-    }
+    public abstract void performAttack();
     /** 
      * {@inheritDoc}
      */
@@ -68,7 +66,7 @@ public abstract class Unit implements Buyable, Combatant {
      */
     @Override
     public int costToUpgrade() {
-        return this.costToUpgrade;
+        return this.costToUpgrade * this.level();
     }
     /** 
      * {@inheritDoc}
@@ -92,8 +90,10 @@ public abstract class Unit implements Buyable, Combatant {
      */
     @Override
     public void downgrade() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'downgrade'");
+        if (this.level == 1) {
+            throw new IllegalStateException();
+        }
+        this.level--;
     }
     /**
      * Method that checks if the object can be upgraded.
