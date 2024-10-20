558bc4a6236f339b6773c94d338b3ed2ce5c3dce
Riccardo Bartolini <riccardo.bartolini5@studio.unibo.it>
2024-10-20 15:47:04 +0200

    Creation of the Cell's interface

---
+++ src/main/java/it/unibo/the100dayswar/model/map/api/Cell.java
@@ -0,0 +1,22 @@
+package it.unibo.the100dayswar.model.map.api;
+/**
+ * Interface a model a  cell
+ */
+public interface Cell {
+/**
+ * getter x postion
+ * @return the orizontal position in te map
+ */
+int getX();
+
+/**
+ * getter y postion
+ * @return the vrtical position in te map
+ */
+int getY();
+
+
+
+
+    
+}
