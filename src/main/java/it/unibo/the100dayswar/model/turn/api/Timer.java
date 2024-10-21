cc082876614df54536874d75804b1efc5cedef59
Filippo <filippo.francalanci@studio.unibo.it>
2024-10-21 16:59:31 +0200

    removed DayTimer and TurnTimer, created Timer interface

--- src/main/java/it/unibo/the100dayswar/model/turn/api/Timer.java
+++ src/main/java/it/unibo/the100dayswar/model/turn/api/Timer.java
@@ -1,13 +1,17 @@
 package it.unibo.the100dayswar.model.turn.api;
 
 /**
- * Interface for the timer in a turn.
+ * Interface for the timer.
  */
-public interface TurnTimer {
+public interface Timer {
     /**
      * function for starting the timer.
      */
     void startTimer();
+    /**
+     * function for stopping the timer.
+     */
+    void stopTimer();
     /**
      * function for resetting the timer.
      */
