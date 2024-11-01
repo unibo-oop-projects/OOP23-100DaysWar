c2abb9256f9c6894b9d51f02d6c67f63e720acf8
Filippo <filippo.francalanci@studio.unibo.it>
2024-11-01 14:49:43 +0100

    removed gameTurn and Timer Interface, Added GameTurnManager Interface,created implemented class GameDayImpl, GameTurnManagerImpl of the interface GameDay and GameTurnManager

--- src/main/java/it/unibo/the100dayswar/model/turn/api/GameTurnManager.java
+++ src/main/java/it/unibo/the100dayswar/model/turn/api/GameTurnManager.java
@@ -5,12 +5,17 @@ import it.unibo.the100dayswar.model.player.api.Player;
 /**
  * An Interface for managing the turn swith between the two player.
  */
-public interface GameTurn {
+public interface GameTurnManager {
     /**
      * get the current player.
      * @return the current player
      */
     Player getCurrentPlayer();
+    /**
+     * get the current player.
+     * @return the index of the current player in the list
+     */
+    int getCurrentPlayerIndex();
     /**
      * get the current turn.
      * @return the current turn
@@ -24,4 +29,22 @@ public interface GameTurn {
      * increase the Turn counter.
      */
     void increaseTurn();
+    /**
+     * check if the game end.
+     * 
+     * @return true if game end
+     */
+    boolean isGameEnd();
+    /**
+     * called when a player start his turn.
+     */
+    void playerStartTurn();
+    /**
+     * unsed to automatically change turn when day passes.
+     */
+    void onDayPassed();
+    /**
+     * start the timer for the day.
+     */
+    void dayTimer();
 }
---
+++ src/main/java/it/unibo/the100dayswar/model/turn/impl/GameTurnManagerImpl.java
@@ -0,0 +1,129 @@
+package it.unibo.the100dayswar.model.turn.impl;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+import it.unibo.the100dayswar.model.player.api.Player;
+import it.unibo.the100dayswar.model.turn.api.GameTurnManager;
+import it.unibo.the100dayswar.model.turn.api.GameDay;
+
+/**
+ * class that implement the interface GameTurnManager.
+ */
+public class GameTurnManagerImpl implements GameTurnManager {
+
+    private static final int PERIOD = 6000;
+    private static final int MAX_TURN_WITH_NO_MOVE = 4;
+    private int turn;
+    private final List<Player> players;
+    private int currentPlayerIndex;
+    private int daysNoMove;
+    private boolean gameEnd;
+    private final GameDay gameDay;
+    private final Timer timer;
+
+    /**
+     * Constructor of GameTurnManagerImpl.
+     * 
+     * @param players List of player
+     */
+    public GameTurnManagerImpl(final List<Player> players) {
+        this.turn = 1;
+        this.players = new ArrayList<>();
+        this.players.addAll(players);
+        this.currentPlayerIndex = 0;
+        this.gameEnd = false;
+        this.daysNoMove = 0;
+        this.gameDay = new GameDayImpl();
+        gameDay.attach(players.get(0));
+        gameDay.attach(players.get(1));
+        timer = new Timer();
+    }
+    /**
+     * get the current player.
+     * 
+     * @return the current player
+     */
+    @Override
+    public Player getCurrentPlayer() {
+        return this.players.get(getCurrentPlayerIndex());
+    }
+    /**
+     * get the current player.
+     * 
+     * @return the index of the current player in the list
+     */
+    @Override
+    public int getCurrentPlayerIndex() {
+        return this.currentPlayerIndex;
+    }
+    /**
+     * get the current turn.
+     * 
+     * @return the current turn
+     */
+    @Override
+    public int getCurrentTurn() {
+        return this.turn;
+    }
+    /**
+     * switch Turn to the other player.
+     */
+    @Override
+    public void switchTurn() {
+        currentPlayerIndex = (currentPlayerIndex == 0) ? 1 : 0;
+        increaseTurn();
+        playerStartTurn();
+    }
+    /**
+     * increase the Turn counter.
+     */
+    @Override
+    public void increaseTurn() {
+        this.turn++;
+    }
+    /**
+     * check if the game end.
+     * @return boolean value if game ends
+     */
+    @Override
+    public boolean isGameEnd() {
+        return this.gameEnd;
+    }
+    /**
+     * called when a player start his turn.
+     */
+    @Override
+    public void playerStartTurn() {
+        this.daysNoMove = 0;
+    }
+    /**
+     * unsed to automatically change turn when day passes.
+     */
+    @Override
+    public void onDayPassed() {
+        this.gameDay.increaseDay();
+        this.daysNoMove++;
+        if (daysNoMove >= MAX_TURN_WITH_NO_MOVE) {
+            switchTurn();
+        }
+        if (gameDay.getCurrentDay() >= gameDay.getMaxDay()) {
+            gameEnd = true;
+            timer.cancel();
+        }
+    }
+    /**
+     * start the timer for the day.
+     */
+    @Override
+    public void dayTimer() {
+        final TimerTask dayTimer = new TimerTask() {
+            @Override
+            public void run() {
+                onDayPassed();
+            }
+        };
+        timer.schedule(dayTimer, 0, PERIOD);
+    }
+}
