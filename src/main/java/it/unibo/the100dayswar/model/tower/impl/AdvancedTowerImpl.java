f2d78cc5cd163c0d51fba816c98754e871264dbd
Riccardo Balzani <riccardo.balzani@studio.unibo.it>
2024-11-03 20:27:59 +0100

    AdvancedTowerImpl created

---
+++ src/main/java/it/unibo/the100dayswar/model/tower/impl/AdvancedTowerImpl.java
@@ -0,0 +1,34 @@
+package it.unibo.the100dayswar.model.tower.impl;
+
+import it.unibo.the100dayswar.model.cell.api.Cell;
+import it.unibo.the100dayswar.model.player.api.Player;
+import it.unibo.the100dayswar.model.tower.api.TowerType;
+
+public class AdvancedTowerImpl extends AbstractTower {
+    private static final int HEALTH_MULTYPLIER_ADVANCED = 12;
+    private static final int UPGRADE_MULTYPLIER_ADVANCED = 2;
+
+    /**
+     * Constructs an advanced tower.
+     * 
+     * @param owner the owner of the advanced tower
+     * @param position the position of the advanced tower in the map
+     */
+    public AdvancedTowerImpl(final Player owner, final Cell position) {
+        super(TowerType.ADVANCED, owner,
+        TowerType.ADVANCED.getPrice() * HEALTH_MULTYPLIER_ADVANCED, 
+        position, 
+        TowerType.ADVANCED.getPrice(), 
+        TowerType.ADVANCED.getPrice() * UPGRADE_MULTYPLIER_ADVANCED);
+    }
+
+    /**
+     * {@inheritDoc}}
+     */
+    @Override
+    public int getDamage() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDamage'");
+    }
+    
+}
--- src/main/java/it/unibo/the100dayswar/model/tower/impl/BasicTowerImpl.java
+++ src/main/java/it/unibo/the100dayswar/model/tower/impl/BasicTowerImpl.java
@@ -5,17 +5,26 @@ import it.unibo.the100dayswar.model.player.api.Player;
 import it.unibo.the100dayswar.model.tower.api.TowerType;
 
 public class BasicTowerImpl extends AbstractTower {
-    private static final int HEALTH_MULTYPLIER = 10;
-    private static final int UPGRADE_MULTYPLIER = 2;
+    private static final int HEALTH_MULTYPLIER_BASIC = 10;
+    private static final int UPGRADE_MULTYPLIER_BASIC = 2;
 
+    /**
+     * Constructs a basic tower.
+     * 
+     * @param owner the owner of the basic tower
+     * @param position the positon of basic tower in the map
+     */
     public BasicTowerImpl(final Player owner, final Cell position) {
         super(TowerType.BASIC, owner,
-        TowerType.BASIC.getPrice() * HEALTH_MULTYPLIER, 
+        TowerType.BASIC.getPrice() * HEALTH_MULTYPLIER_BASIC , 
         position, 
         TowerType.BASIC.getPrice(), 
-        TowerType.BASIC.getPrice() * UPGRADE_MULTYPLIER);
+        TowerType.BASIC.getPrice() * UPGRADE_MULTYPLIER_BASIC);
     }
 
+    /**
+     * {@inheritDoc}}
+     */
     @Override
     public int getDamage() {
         // TODO Auto-generated method stub
