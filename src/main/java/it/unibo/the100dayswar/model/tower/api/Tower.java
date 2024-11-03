ddd3debf4539bbdc88c95e4b99a78eb03c1ad5e9
Riccardo Balzani <riccardo.balzani@studio.unibo.it>
2024-11-03 17:03:40 +0100

    AbstractTower updated

--- src/main/java/it/unibo/the100dayswar/model/tower/api/Tower.java
+++ src/main/java/it/unibo/the100dayswar/model/tower/api/Tower.java
@@ -1,9 +1,31 @@
 package it.unibo.the100dayswar.model.tower.api;
 
-import it.unibo.the100dayswar.model.map.api.Cell;
+import it.unibo.the100dayswar.model.cell.api.Cell;
 import it.unibo.the100dayswar.model.unit.api.Unit;
 
+/**
+ * Interface that rapresent a generic tower.
+ */
 public interface Tower extends Unit {
-    int getDamege();
+    /**
+     * Method that returns the damage that the tower inflicts
+     * to another unit. 
+     * 
+     * @return the damage
+     */
+    int getDamage();
+
+    /**
+     * Method that returns the position in the map of the tower.
+     * 
+     * @return the position
+     */
     Cell getCell();
+
+    /**
+     * Method that return the type of the tower
+     * 
+     * @return the tower type
+     */
+    TowerType getTowerType();
 }
--- src/main/java/it/unibo/the100dayswar/model/tower/impl/AbstractTower.java
+++ src/main/java/it/unibo/the100dayswar/model/tower/impl/AbstractTower.java
@@ -1,8 +1,6 @@
 package it.unibo.the100dayswar.model.tower.impl;
 
-import it.unibo.the100dayswar.commons.utilities.api.Position;
-import it.unibo.the100dayswar.commons.utilities.impl.PositionImpl;
-import it.unibo.the100dayswar.model.map.api.Cell;
+import it.unibo.the100dayswar.model.cell.api.Cell;
 import it.unibo.the100dayswar.model.player.api.Player;
 import it.unibo.the100dayswar.model.tower.api.Tower;
 import it.unibo.the100dayswar.model.tower.api.TowerType;
@@ -14,7 +12,8 @@ import it.unibo.the100dayswar.model.unit.impl.UnitImpl;
 public abstract class AbstractTower extends UnitImpl implements Tower {
     private static final int MAX_LEVEL = 4;
     private final TowerType towerType;
-    
+    private final Cell position;
+
     /**
      * Constructs a Tower with the specified position, level, and tower type.
      *
@@ -22,10 +21,30 @@ public abstract class AbstractTower extends UnitImpl implements Tower {
      * @param level     the level of the tower
      * @param towerType the type of the tower
      */
-    public AbstractTower(final TowerType towerType, final Player owner, final Cell position, final int costToBuy, final int costToUpgrade) {
-        super(owner, costToBuy, costToUpgrade, MAX_LEVEL, position);
+    public AbstractTower(final TowerType towerType, final Player owner, final int health, final Cell position, final int costToBuy, final int costToUpgrade) {
+        super(owner, health, costToBuy, costToUpgrade, MAX_LEVEL);
+        this.position = position;
         this.towerType = towerType;
     }
 
+    @Override
+    public void performAttack() {
+        // TODO Auto-generated method stub
+        
+    }
 
+    @Override
+    public Cell getCell() {
+        return this.position;
+    }
+
+    @Override
+    public abstract int getDamage();
+
+    @Override
+    public TowerType getTowerType() {
+        return this.towerType;
+    }
+
+    
 }
