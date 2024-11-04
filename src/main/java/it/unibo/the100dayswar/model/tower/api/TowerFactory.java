bca7528bea62c2e2764b503aed98d96f7000ffe9
Riccardo Balzani <riccardo.balzani@studio.unibo.it>
2024-11-04 01:20:46 +0100

    ToweFactoryImpl and TowerFactory implemented

--- src/main/java/it/unibo/the100dayswar/model/tower/api/TowerFactory.java
+++ src/main/java/it/unibo/the100dayswar/model/tower/api/TowerFactory.java
@@ -3,21 +3,19 @@ package it.unibo.the100dayswar.model.tower.api;
 import it.unibo.the100dayswar.model.cell.api.Cell;
 import it.unibo.the100dayswar.model.player.api.Player;
 import it.unibo.the100dayswar.model.tower.impl.AbstractTower;
-import it.unibo.the100dayswar.model.tower.impl.AdvancedTowerImpl;
-import it.unibo.the100dayswar.model.tower.impl.BasicTowerImpl;
 
+/**
+ * Interface that rapresent a TowerFactory.
+ */
 public interface TowerFactory {
-    static AbstractTower buildTower(final Player owner,final TowerType towerType, final Cell position) {
-        switch (towerType){
-            case BASIC:
-                return new BasicTowerImpl(owner, position);
-
-            case ADVANCED:
-                return new AdvancedTowerImpl(owner, position);
-            
-            default:
-                throw new IllegalArgumentException("Tower Type unknow");
-
-        }
-    };
+    /**
+     * Method that call the constructor which is corrispondent to towerType.
+     * 
+     * @param owner the owner of the tower
+     * @param towerType the type of the tower
+     * @param position the position of the tower
+     * 
+     * @return a new tower
+     */
+    public AbstractTower buildTower(final Player owner, final TowerType towerType, final Cell position);
 }
--- src/main/java/it/unibo/the100dayswar/model/tower/impl/TowerFactoryImpl.java
+++ src/main/java/it/unibo/the100dayswar/model/tower/impl/TowerFactoryImpl.java
@@ -1,12 +1,37 @@
 package it.unibo.the100dayswar.model.tower.impl;
 
+import java.util.Map;
+import java.util.function.BiFunction;
+
 import it.unibo.the100dayswar.model.cell.api.Cell;
 import it.unibo.the100dayswar.model.player.api.Player;
 import it.unibo.the100dayswar.model.tower.api.TowerFactory;
 import it.unibo.the100dayswar.model.tower.api.TowerType;
 
+/**
+ * Class that implements a factory pattern for all differents kind of towers.
+ */
 public class TowerFactoryImpl implements TowerFactory {
-    public static AbstractTower buildTower (final Player owner, final TowerType towerType, final Cell position){
-        return TowerFactoryImpl.buildTower(owner, towerType, position);
-    }
+
+    /**
+     * Contains the tower type associated with its constructor.
+     */
+    private static final Map<TowerType, BiFunction<Player, Cell, AbstractTower>> TOWER_CREATORS = Map.of(
+        TowerType.BASIC, BasicTowerImpl::new,
+        TowerType.ADVANCED, AdvancedTowerImpl::new
+    );
+
+    /**
+     * {@inheritDoc}}
+     */
+    @Override
+    public AbstractTower buildTower(Player owner, TowerType towerType, Cell position) {
+        BiFunction<Player, Cell, AbstractTower> towerConstructor = TOWER_CREATORS.get(towerType);
+
+        if (towerConstructor == null) {
+            throw new IllegalArgumentException("Unknown Tower Type: " + towerType);
+        }
+
+        return towerConstructor.apply(owner, position);
+    } 
 }
