b3398370f19b57d287c95a8c44ab71f1e80a5ee7
Riccardo Bartolini <riccardo.bartolini5@studio.unibo.it>
2024-11-09 14:06:17 +0100

    modify interface unit and class unitImpl to improve the use of Observer

--- src/main/java/it/unibo/the100dayswar/model/unit/api/Unit.java
+++ src/main/java/it/unibo/the100dayswar/model/unit/api/Unit.java
@@ -22,4 +22,11 @@ public interface Unit extends Buyable, Combatant, Observable<Pair<Unit, Cell>> {
   * @return the current cell of the unit
   */
   Cell getPosition();
+
+  /**
+     * Notifies all observers of this Soldier that it has moved to a new cell.
+     * 
+     * @param target the cell the Soldier has moved to
+     */
+    void notifyObservers(Cell target);
 }
