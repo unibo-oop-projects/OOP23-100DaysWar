ce3435f392ef234f36344ae1a1c390ed09e69da1
Riccardo Bartolini <riccardo.bartolini5@studio.unibo.it>
2024-11-14 15:52:39 +0100

    added two method in class GameStastistic and GameStatisticImpl

--- src/main/java/it/unibo/the100dayswar/model/statistic/api/GameStatistics.java
+++ src/main/java/it/unibo/the100dayswar/model/statistic/api/GameStatistics.java
@@ -1,8 +1,22 @@
 package it.unibo.the100dayswar.model.statistic.api;
 
+import it.unibo.the100dayswar.model.player.api.Player;
+
 /**
  * Interface that model the game statistics.
  */
 public interface GameStatistics {
+
+    /** 
+     * Get the number of soldiers of a player.
+     * @param player the player.
+     */
+    int getSoldiers(Player player);
+
+    /**
+     * Get the number of towers of a player.
+     * @param player the player.
+     */
+    int getTower(Player player);
 
 }
--- src/main/java/it/unibo/the100dayswar/model/statistic/impl/GameStatisticImpl.java
+++ src/main/java/it/unibo/the100dayswar/model/statistic/impl/GameStatisticImpl.java
@@ -1,9 +1,28 @@
 package it.unibo.the100dayswar.model.statistic.impl;
 
+import it.unibo.the100dayswar.model.player.api.Player;
 import it.unibo.the100dayswar.model.statistic.api.GameStatistics;
 /**
  * The implementation of the game statistics.
  */
 public class GameStatisticImpl implements GameStatistics {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getSoldiers(Player player) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getSoldiers'");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getTower(Player player) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getTower'");
+    }
 
 }
