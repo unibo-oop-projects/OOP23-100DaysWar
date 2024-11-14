059c555681fb0aafb56e659c66ce923090db06e3
gimbo <gianmarco.fabbri3@studio.unibo.it>
2024-11-14 10:30:24 +0100

    Introduce a utility class for the BotStrategy that encapsulate the logic to evaluate every action that can be done by the bot

---
+++ src/main/java/it/unibo/the100dayswar/model/bot/api/DecisionMaker.java
@@ -0,0 +1,25 @@
+package it.unibo.the100dayswar.model.bot.api;
+
+import java.io.Serializable;
+import java.util.Optional;
+
+import it.unibo.the100dayswar.model.bot.impl.ActionType;
+
+/** 
+ * A utility object that helps the evaluation of the best move 
+ * in the strategy for the bot.
+ */
+public interface DecisionMaker extends Serializable {
+    /**
+     * Evaluates the possible moves.
+     * 
+     * @param botPlayer the bot player
+     */
+    void evaluateMoves(BotPlayer botPlayer);
+    /**
+     * Selects the best move based on the highest score.
+     * 
+     * @return an optional of the best move type or empty if no move is performable
+     */
+    Optional<ActionType> getBestMoveType();
+}
