0acccf1efb43945a41b70503a982b8f795ee26b0
gimbo <gianmarco.fabbri3@studio.unibo.it>
2024-11-21 15:13:25 +0100

    The implementation is finished by including the case where more than one actionType has the same score

--- src/main/java/it/unibo/the100dayswar/model/bot/impl/DecisionMakerImpl.java
+++ src/main/java/it/unibo/the100dayswar/model/bot/impl/DecisionMakerImpl.java
@@ -1,5 +1,6 @@
 package it.unibo.the100dayswar.model.bot.impl;
 
+import java.util.Comparator;
 import java.util.EnumMap;
 import java.util.Map;
 import java.util.Optional;
@@ -8,27 +9,31 @@ import it.unibo.the100dayswar.commons.utilities.impl.Score;
 import it.unibo.the100dayswar.model.bot.api.BotPlayer;
 import it.unibo.the100dayswar.model.bot.api.DecisionMaker;
 
-/** 
+/**
  * Implementation of the decision maker.
  */
 public class DecisionMakerImpl implements DecisionMaker {
     private static final long serialVersionUID = 1L;
-    private static final int NON_PERFORMABLE_SCORE = -1;
     private final Map<ActionType, Score> scoreMove;
+
     /**
      * Constructor of the class.
      */
     public DecisionMakerImpl() {
         this.scoreMove = new EnumMap<>(ActionType.class);
     }
+
     /**
      * {@inheritDoc}
      */
     @Override
     public void evaluateMoves(final BotPlayer botPlayer) {
-        scoreMove.clear(); // Placeholder
-        scoreMove.put(ActionType.PURCHASE_SOLDIER, new Score(NON_PERFORMABLE_SCORE)); // Placeholder
+        scoreMove.clear();
+        for (final ActionType actionType : ActionType.values()) {
+            scoreMove.put(actionType, actionType.evaluate(botPlayer));
+        }
     }
+
     /**
      * {@inheritDoc}
      */
@@ -36,7 +41,8 @@ public class DecisionMakerImpl implements DecisionMaker {
     public Optional<ActionType> getBestMoveType() {
         return scoreMove.entrySet().stream()
                 .filter(entry -> entry.getValue().getValue() >= 0)
-                .max(Map.Entry.comparingByValue())
+                .max(Comparator.<Map.Entry<ActionType, Score>>comparingInt(e -> e.getValue().getValue())
+                        .thenComparing(Map.Entry::getKey))
                 .map(Map.Entry::getKey);
     }
 }
