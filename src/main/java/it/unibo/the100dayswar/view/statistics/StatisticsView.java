b259837f7f7f4c5054cfc3c6d5e4f63eca1d16b8
Riccardo Bartolini <riccardo.bartolini5@studio.unibo.it>
2024-12-29 12:13:03 +0100

    created the StatisticView

---
+++ src/main/java/it/unibo/the100dayswar/view/statistics/StatisticsView.java
@@ -0,0 +1,18 @@
+package it.unibo.the100dayswar.view.statistics;
+
+import it.unibo.the100dayswar.application.The100DaysWar;
+import it.unibo.the100dayswar.controller.statisticContoller.api.StatisticController;
+import it.unibo.the100dayswar.model.player.api.Player;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+
+public class StatisticsView extends JPanel {
+
+    public StatisticsView(){
+        final StatisticController statisticController = The100DaysWar.CONTROLLER.getStatisticController();
+       
+    }
+
+}
