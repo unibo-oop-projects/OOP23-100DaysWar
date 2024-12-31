25696c3f66fe54e476c8d4d19c2136f295efe906
Riccardo Bartolini <riccardo.bartolini5@studio.unibo.it>
2024-12-31 13:44:56 +0100

    created the controller for che map

---
+++ src/main/java/it/unibo/the100dayswar/controller/mapcontroller/api/MapController.java
@@ -0,0 +1,8 @@
+package it.unibo.the100dayswar.controller.mapcontroller.api;
+
+/**
+ * Interface that models the map controller.
+ */
+public interface MapController {
+
+}
---
+++ src/main/java/it/unibo/the100dayswar/controller/mapcontroller/impl/MapControllerIImpl.java
@@ -0,0 +1,10 @@
+package it.unibo.the100dayswar.controller.mapcontroller.impl;
+
+import it.unibo.the100dayswar.controller.mapcontroller.api.MapController;
+
+/**
+ * The implementation of the map controller.
+ */
+public class MapControllerIImpl implements MapController {
+
+}
