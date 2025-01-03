66f5759a22d4dc35e23a7919077baf445c97f4f4
Riccardo Balzani <riccardo.balzani@studio.unibo.it>
2025-01-03 23:43:45 +0100

    ViewUpdater class created

---
+++ src/main/java/it/unibo/the100dayswar/view/viewupdater/ViewUpdater.java
@@ -0,0 +1,13 @@
+package it.unibo.the100dayswar.view.viewupdater;
+
+/**
+ * A class that periodically reloads the map background image on a separate thread.
+ */
+public final class ViewUpdater implements Runnable {
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
+}
