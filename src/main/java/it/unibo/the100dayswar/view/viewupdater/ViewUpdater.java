195043c86b8f2b87e9298a5cd591e30be86d5a9d
Riccardo Balzani <riccardo.balzani@studio.unibo.it>
2025-01-04 17:51:28 +0100

    ViewUpdater class sketched and partially implemented

--- src/main/java/it/unibo/the100dayswar/view/viewupdater/ViewUpdater.java
+++ src/main/java/it/unibo/the100dayswar/view/viewupdater/ViewUpdater.java
@@ -1,5 +1,6 @@
 package it.unibo.the100dayswar.view.viewupdater;
 
+import it.unibo.the100dayswar.application.The100DaysWar;
 import it.unibo.the100dayswar.view.gameview.GameView;
 import it.unibo.the100dayswar.view.map.MapView;
 import it.unibo.the100dayswar.view.statistics.StatisticsView;
@@ -8,19 +9,54 @@ import it.unibo.the100dayswar.view.statistics.StatisticsView;
  * A class that periodically reloads the map background image on a separate thread.
  */
 public class ViewUpdater implements Runnable {
-    private final GameView mainframe;
+    private static final long REFRESH_INTERVAL_MS = 50;
     private final MapView mapView;
     private final StatisticsView statisticsView;
+    private boolean running;
 
     public ViewUpdater(final GameView mainframe) {
-        this.mainframe = mainframe;
         this.mapView = mainframe.getMapView();
         this.statisticsView = mainframe.getStatisticView(); 
+        this.running = false;
+    }
+
+    public void start() {
+        if(!running) {
+            running = true;
+            new Thread(this, "MapImageReloader-Thread").start();
+        }
+    }
+
+    public void stop() {
+        if(running) {
+            running = false;
+        }
     }
 
     @Override
     public void run() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'run'");
+        while (running) {
+            try {
+                // Reload the image
+
+                /*
+                 * TODO aggiungere l'aggiornamento delle posizioni
+                 * The100DaysWar.CONTROLLER.
+                 */
+
+                mapView.paintSoldiers();
+                mapView.paintTowers();
+                
+                mapView.paintStatistics();
+                mapView.paintArea();
+
+                // Sleep until next update
+                Thread.sleep(REFRESH_INTERVAL_MS);
+
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                break;
+            }
+        }
     }
 }
