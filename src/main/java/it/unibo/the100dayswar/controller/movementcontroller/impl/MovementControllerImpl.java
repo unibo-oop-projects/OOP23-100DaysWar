68b50a7ca2203b0dfefd7daea6ae09d890632143
gimbo <gianmarco.fabbri3@studio.unibo.it>
2025-01-05 17:40:19 +0100

    Added the movement of a Unit

--- src/main/java/it/unibo/the100dayswar/controller/movementcontroller/impl/MovementControllerImpl.java
+++ src/main/java/it/unibo/the100dayswar/controller/movementcontroller/impl/MovementControllerImpl.java
@@ -1,42 +1,76 @@
 package it.unibo.the100dayswar.controller.movementcontroller.impl;
 
+import it.unibo.the100dayswar.application.The100DaysWar;
+
+import it.unibo.the100dayswar.commons.utilities.impl.Direction;
+import it.unibo.the100dayswar.commons.utilities.impl.Pair;
+import it.unibo.the100dayswar.commons.utilities.impl.PositionImpl;
 import it.unibo.the100dayswar.controller.movementcontroller.api.MovementController;
+import it.unibo.the100dayswar.model.cell.api.Cell;
+import it.unibo.the100dayswar.model.cell.impl.CellImpl;
+import it.unibo.the100dayswar.model.soldier.api.Soldier;
+import it.unibo.the100dayswar.model.unit.api.Unit;
 
 /**
  * The implementation of the movement controller of the game.
  */
 public class MovementControllerImpl implements MovementController {
-
     /** 
      * {@inheritDoc}
      */
     @Override
     public void moveUp() {
+        move(Direction.UP);
     }
+    
     /** 
      * {@inheritDoc}
      */
     @Override
     public void moveDown() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'moveDown'");
+        move(Direction.DOWN);
     }
-
+    
     /** 
      * {@inheritDoc}
      */
     @Override
     public void moveLeft() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'moveLeft'");
+        move(Direction.LEFT);
     }
-
+    
     /** 
      * {@inheritDoc}
      */
     @Override
     public void moveRight() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'moveRight'");
+        move(Direction.RIGHT);
+    }
+    
+    /**
+     * Moves the selected soldier in the specified direction.
+     * @param direction the direction in which the soldier should move
+     */
+    private void move(Direction direction) {
+        final Pair<Unit, Cell> selectedCell = The100DaysWar.CONTROLLER.getMapController().getSelectedCell();
+        final Unit unit = selectedCell.getFirst();
+        final Cell currentCell = selectedCell.getSecond();
+    
+        if (unit != null && unit instanceof Soldier) {
+            int targetX = currentCell.getPosition().getX();
+            int targetY = currentCell.getPosition().getY();
+            switch (direction) {
+                case UP -> targetY -= 1;
+                case DOWN -> targetY += 1;
+                case LEFT -> targetX -= 1;
+                case RIGHT -> targetX += 1;
+            }
+            
+            ((Soldier) unit).movementRequest(new CellImpl(new PositionImpl(targetX, targetY), currentCell.isBuildable(), currentCell.isSpawn()));
+        } else {
+            throw new UnsupportedOperationException(
+                unit == null ? "No unit selected" : "The unit selected is not a soldier"
+            );
+        }
     }
 }
