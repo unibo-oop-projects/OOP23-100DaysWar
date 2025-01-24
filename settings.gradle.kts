29f33c041114f6b97dd922f8d6f29f15086299f0
Danilo Pianini <danilo.pianini@unibo.it>
2025-01-24 16:58:17 +0100

    ci: add the OOP machinery

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,16 @@
-rootProject.name = "OOP23-100DaysWar"
+rootProject.name = "oop-24-100DaysWar"
+plugins {
+    id("com.gradle.develocity") version "3.19.1"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+}
+
+develocity {
+    buildScan {
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
+        uploadInBackground = !System.getenv("CI").toBoolean()
+        buildScanPublished {
+            file("scan-journal.log").writeText("$buildScanId - $buildScanUri\n")
+        }
+    }
+}
